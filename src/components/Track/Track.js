f43b8cc3ebe84c1d0ae7c60873e8ff99e968f0b1
Oisin Quinn <oisin1001@gmail.com>
2018-08-19 16:37:44 +0100

    Added scroll icon, added entry number, tidied design

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Arrow from '../../images/scroll.svg'
 import './Track.css';
 
 const Track = () => {
     return(
     <div className="track">
-        <span className="songTitle">Rainy Night in Slomo</span>
-        <span className="artist">We Cut Corners</span>
-        <div>
-            <iframe src="https://open.spotify.com/embed/track/2wq2KG5bc2tjXecYpfcbzT" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media" title="play" aria-hidden="true"></iframe>
+        <span className="entryNumber flexElement">#001</span>
+        <span className="songTitle flexElement">Rainy Night in Slomo</span>
+        <span className="artist flexElement">We Cut Corners</span>
+        <div className="flexElement">
+        <iframe src="https://open.spotify.com/embed/track/2wq2KG5bc2tjXecYpfcbzT" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        </div>
+        <div className="flexElement arrow">
+            <img src={Arrow} width="63"></img>
         </div>
     </div>);
 }
