cc29a67dec3e814ba1f23fcc7f1365b7cc38e035
Oisin Quinn <oisin1001@gmail.com>
2018-08-19 13:40:12 +0100

    Started work on the Track component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Description from './components/Description/Description';
 import Track from './components/Track/Track';
+import Background from './images/corners_white.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Track />
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" /> 
+        <Track className="track"/>
         <Description />
       </div>
     );
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './Track.css';
 
 const Track = () => {
-    return(<div>Track goes here</div>);
+    return(
+    <div className="track">
+        <span className="songTitle">Rainy Night in Slomo</span>
+        <br></br>
+        <span className="artist">We Cut Corners</span>
+    </div>);
 }
 
 export default Track;
