130a4373f41f5432acd76b8f5c18b22dae883ffa
Oisin Quinn <oisin1001@gmail.com>
2018-08-19 15:49:36 +0100

    Started using flexbox, added Spotify widget

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Description from './components/Description/Description';
 import Track from './components/Track/Track';
-import Background from './images/corners_white.jpg';
 import './App.css';
+
+window.onresize = function(){
+  document.body.height = window.innerHeight;
+}
+window.onresize(); // called to initially set the height.
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" /> 
+        {/* <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />  */}
         <Track className="track"/>
         <Description />
       </div>
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -5,8 +5,10 @@ const Track = () => {
     return(
     <div className="track">
         <span className="songTitle">Rainy Night in Slomo</span>
-        <br></br>
         <span className="artist">We Cut Corners</span>
+        <div>
+            <iframe src="https://open.spotify.com/embed/track/2wq2KG5bc2tjXecYpfcbzT" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media" title="play" aria-hidden="true"></iframe>
+        </div>
     </div>);
 }
 
