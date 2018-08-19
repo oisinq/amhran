050f86d1fe48799244cc899f82b73905d6511069
Oisin Quinn <oisin1001@gmail.com>
2018-08-19 18:07:39 +0100

    Added track description

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './Description.css';
 
 const Description = () => {
-    return(<div>Hi</div>);
+    return(
+    <div className="main">
+        <span className="firstParagraph">We Cut Corners' latest is a predictably complex meditation, with religious imagery and vignettes of a stagnating relationship blending beautifully.</span>
+        <span className="secondParagraph">Sounding absolutely nothing like the Pogues song whose name it plays on, We Cut Corners' latest release 'Rainy Night In Slomo' is a quietly building track which ends climactically (not to mention brilliantly) with the lines 'Don't mean to be abrupt/But can we focus on not fucking this one up?" It's simple guitar and drum rhythms help slowly ratchet up the intensity of the song. What it's really about is anyone's guess.</span>
+    </div>
+);
 }
 
 export default Description;
