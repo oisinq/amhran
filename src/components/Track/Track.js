4bb972d2ad96f017566c3681c59dc052e6c4d0af
Oisin Quinn <oisin1001@gmail.com>
2018-08-24 16:49:24 +0100

    Website now reads background image and gradient info from tracks.json

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Arrow from '../../images/scroll.svg'
+
 import './Track.css';
 
 const Track = ({track}) => {
     let embed;
-    
+
     if (track.mediaType === "spotify") {
         embed = <iframe src={track.mediaLink} title="Track of the day" width="300px" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
     } else if (track.mediaType === "bandcamp") {
@@ -12,10 +13,18 @@ const Track = ({track}) => {
     } else if (track.mediaType === "soundcloud") {
         console.log("I got nothin'!");
     }
-    console.log(embed);
+    console.log(track.style);
 
+    let imager = require(`../../images/${track.image}`);
+
+    let backgroundStyle = {
+        "background": `linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0) 30%, rgba(0,0,0,1) 100%), ${track.style.background}, url("${imager}")`,
+        "background-size": "cover",
+        "background-position": "center"
+    }
+    
     return(
-        <div className="track">
+        <div style={backgroundStyle} className="track">
             <span className="entryNumber flexElement">{track.order}</span>
             <span className="songTitle flexElement">{track.songName}</span>
             <span className="artist flexElement">{track.artistName}</span>
