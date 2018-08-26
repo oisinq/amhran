fbd12816b35bff7e7d592f9d5e7a94c8e718acdc
Oisin Quinn <oisin1001@gmail.com>
2018-08-26 17:18:34 +0100

    Added link to logo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,18 +5,18 @@ import './Nav.css';
 const Nav = ({origin}) => {
     let content;
     if (origin === "archive") {
-      content = <div id="navWrapper"><div class="navLink"><Link to="/" style={{ textDecoration: 'none' }}>Today</Link></div></div>;  
+      content = <div id="navWrapper"><div class="navLink"><Link to="/" className="link">Today</Link></div></div>;  
     } else if (origin === "main") {
-        content = <div id="navWrapper"><div class="navLink"><Link to="/archive/" style={{ textDecoration: 'none' }}>Archive</Link></div></div>
+        content = <div id="navWrapper"><div class="navLink"><Link to="/archive/" className="link">Archive</Link></div></div>
     }else {
-        content = <div id="navWrapper"><div class="navLink"><Link to="/archive/" style={{ textDecoration: 'none' }}>Archive</Link></div>
-        <div class="navLink"><Link to="/" style={{ textDecoration: 'none' }}>Today</Link></div>
+        content = <div id="navWrapper"><div class="navLink"><Link to="/archive/" className="link">Archive</Link></div>
+        <div class="navLink"><Link to="/" className="link">Today</Link></div>
         </div>
     }
 
     return(
     <nav className="Nav">
-        <span id="logo">amhran.ie</span>
+        <span id="logo"><Link to="/" className="link">amhran.ie</Link></span>
         {content}
     </nav>
 );
