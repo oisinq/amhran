2bbfd97accf88b9c53e8000d12423221240985e2
Oisin Quinn <oisin1001@gmail.com>
2018-09-11 22:57:42 +0100

    Added launch screen

---
+++ src/components/Launch/Launch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Launch.css';
+
+const Launch = () => {
+    
+    return(
+        <div>
+        <h1>Launching Thursday 13th September.</h1>
+        <h2>One new Irish song. Every day.</h2>
+    </div>);
+    }
+
+export default Launch;
