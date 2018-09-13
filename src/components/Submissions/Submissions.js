a6cfb124796240e2c500ab31d8315b349cff1092
Oisin Quinn <oisin1001@gmail.com>
2018-09-13 13:13:56 +0100

    Changed submission form

--- src/components/Submissions/Submissions.js
+++ src/components/Submissions/Submissions.js
@@ -7,7 +7,6 @@ const Submissions = () => {
         <form className="mainy pure-form pure-form-stacked" method="POST" action="https://formspree.io/info@amhran.ie">
             <input className="formElement" name="track" placeholder="Song name and artist" type="text"/>
             <input className="formElement" name="link" placeholder="Link (Soundcloud/Bandcamp preferred)" type="text"/>
-            <input className="formElement" name="artistSocial" placeholder="Artist's social media name" type="text"/>
             <input className="formElement" name="email" placeholder="Your email" type="email"/>
             <textarea className="formElement" name="message" placeholder="Extra details/requests"></textarea>
             <button className="formElement pure-button pure-button-primary" type="submit">Send</button>
