87f6afeda0c2ea2592aa1a976c1edcd7d963d7c5
Oisin Quinn <oisin1001@gmail.com>
2018-09-15 23:00:17 +0100

    Updated 005 description

--- src/components/Submissions/Submissions.js
+++ src/components/Submissions/Submissions.js
@@ -6,7 +6,7 @@ const Submissions = () => {
     <div>
         <form className="mainy pure-form pure-form-stacked" method="POST" action="https://formspree.io/info@amhran.ie">
             <input className="formElement" name="track" placeholder="Song name and artist" type="text"/>
-            <input className="formElement" name="link" placeholder="Link (Soundcloud/Bandcamp preferred)" type="text"/>
+            <input className="formElement" name="link" placeholder="Link (We'll share whichever streaming site you use)" type="text"/>
             <input className="formElement" name="email" placeholder="Your email" type="email"/>
             <textarea className="formElement" name="message" placeholder="Extra details/requests"></textarea>
             <button className="formElement pure-button pure-button-primary" type="submit">Send</button>
