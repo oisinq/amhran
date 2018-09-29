24d6e5643a10c09a612ca6a226242205fcee47d7
Oisin Quinn <oisin1001@gmail.com>
2018-09-29 14:43:34 +0100

    Added test.html to help Netlify bots recognise the form, updated form JSX

--- src/components/Submissions/Submissions.js
+++ src/components/Submissions/Submissions.js
@@ -4,7 +4,8 @@ import './Submissions.css';
 const Submissions = () => {
     return(
     <div>
-        <form className="mainy pure-form pure-form-stacked" method="POST" name="musicSubmission" netlify>
+        <form className="mainy pure-form pure-form-stacked" method="POST" name="musicSubmission">
+            <input type="hidden" name="form-name" value="musicSubmission" />
             <input className="formElement" name="track" placeholder="Song name and artist" type="text"/>
             <input className="formElement" name="link" placeholder="Link (We'll share whichever streaming site you use)" type="text"/>
             <input className="formElement" name="email" placeholder="Your email" type="email"/>
