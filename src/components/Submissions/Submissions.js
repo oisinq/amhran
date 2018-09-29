0f29b3968429ce60ca6366c3e6983e3ab30f20cd
Oisin Quinn <oisin1001@gmail.com>
2018-09-29 14:32:50 +0100

    Removed Formspree from form

--- src/components/Submissions/Submissions.js
+++ src/components/Submissions/Submissions.js
@@ -4,7 +4,7 @@ import './Submissions.css';
 const Submissions = () => {
     return(
     <div>
-        <form className="mainy pure-form pure-form-stacked" method="POST" action="https://formspree.io/info@amhran.ie" netlify>
+        <form className="mainy pure-form pure-form-stacked" method="POST" name="musicSubmission" netlify>
             <input className="formElement" name="track" placeholder="Song name and artist" type="text"/>
             <input className="formElement" name="link" placeholder="Link (We'll share whichever streaming site you use)" type="text"/>
             <input className="formElement" name="email" placeholder="Your email" type="email"/>
