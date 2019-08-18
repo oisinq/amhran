59885a0d1796f2f44ccd8a7b0dd7ebabed0cd7cb
JR Leonard <allanleonardjr@gmail.com>
2019-08-17 22:33:44 -0400

    Adding SEO component which fixes Issue # 11 by adding Open Graph (facebook) and Twitter meta tags. https://developers.facebook.com/docs/opengraph/music/ https://css-tricks.com/essential-meta-tags-social-media/

---
+++ src/components/SEO/SEO.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Helmet} from 'react-helmet';
+
+const SEO = props => {
+  const {title, description, url, image, type, siteName, twitterCard} = props;
+
+  return (
+    <Helmet>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="og:type" content={type} />
+      <meta property="og:site_name" content={siteName} />
+      <meta property="twitter:card" content={twitterCard} />
+    </Helmet>
+  );
+};
+
+export default SEO;
+
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  url: PropTypes.string,
+  image: PropTypes.string,
+  type: PropTypes.string,
+  siteName: PropTypes.string,
+  twitterCard: PropTypes.string,
+};
+
+SEO.defaultProps = {
+  title: '',
+  description: '',
+  url: '',
+  image: '',
+  type: 'music.song',
+  siteName: 'Amhrán',
+  twitterCard: 'summary_large_image',
+};
